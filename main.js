c028e76e9d2f12e6c4d28b6a91fd22cc4fe32a7f
Srinithi <88095017+srinithi2@users.noreply.github.com>
2022-01-20 09:42:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-https://teachablemachine.withgoogle.com/models/Zy_HELOKg/
+function startClassifictaion() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Zy_HELOKg/',modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResult);
+}
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
